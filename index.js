2e9d1a6a5457dba51b0dfc08938d68f52fd8fa61
Muhammad Zaheer Nazir <m.zaheer.nazir@gmail.com>
2024-07-13 01:35:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,195 @@
+const _0x267948 = _0x42b1;
+(function (_0x393fcf, _0x4cd75b) {
+  const _0xb3790a = _0x42b1,
+  _0x9d637f = _0x393fcf();
+  while (!![]) {
+    try {
+      const _0x407b49 = - parseInt(_0xb3790a(266)) / 1 + parseInt(_0xb3790a(261)) / 2 * (parseInt(_0xb3790a(274)) / 3) + parseInt(_0xb3790a(262)) / 4 + - parseInt(_0xb3790a(263)) / 5 + parseInt(_0xb3790a(223)) / 6 * (parseInt(_0xb3790a(234)) / 7) + - parseInt(_0xb3790a(233)) / 8 + - parseInt(_0xb3790a(265)) / 9;
+      if (_0x407b49 === _0x4cd75b) break;
+       else _0x9d637f['push'](_0x9d637f['shift']());
+    } catch (_0x2c5519) {
+      _0x9d637f['push'](_0x9d637f['shift']());
+    }
+  }
+}(_0x5f26, 858367));
+const socket = io(_0x267948(244));
+function _0x42b1(_0x5c1e5b, _0x36d94d) {
+  const _0x5f26ef = _0x5f26();
+  return _0x42b1 = function (_0x42b1b0, _0x228538) {
+    _0x42b1b0 = _0x42b1b0 - 223;
+    let _0x5211b3 = _0x5f26ef[_0x42b1b0];
+    return _0x5211b3;
+  },
+  _0x42b1(_0x5c1e5b, _0x36d94d);
+}
+let authYaml = ![],
+authNostromo = ![];
+const yamlSocket = io('/yaml'),
+nostromoSocket = io(_0x267948(225)),
+authWebSocket = () =>{
+  const _0x156a2c = _0x267948;
+  yamlSocket['on'](_0x156a2c(228), () =>{
+    const _0x4307e0 = _0x156a2c;
+    console[_0x4307e0(246)](_0x4307e0(267));
+  }),
+  yamlSocket['on'](_0x156a2c(269), _0x4cb35d=>{
+    authYaml = !![];
+    if (authNostromo && authYaml) modifyData();
+  }),
+  yamlSocket['on']('disconnect', () =>{
+    const _0x39da50 = _0x156a2c;
+    console['log'](_0x39da50(273));
+  }),
+  nostromoSocket['on'](_0x156a2c(228), () =>{
+    const _0x502a33 = _0x156a2c;
+    console[_0x502a33(246)](_0x502a33(256));
+  }),
+  nostromoSocket['on'](_0x156a2c(247), _0x48d569=>{
+    authNostromo = !![];
+    if (authNostromo && authYaml) modifyData();
+  }),
+  nostromoSocket['on'](_0x156a2c(272), () =>{
+    const _0x29c790 = _0x156a2c;
+    console[_0x29c790(246)](_0x29c790(226));
+  });
+};
+authWebSocket();
+const modifyData = () =>{
+  const _0x3f3ee3 = _0x267948;
+  contentx[2] = _0x3f3ee3(240),
+  contentx[3] = atob(_0x3f3ee3(236)),
+  document['querySelector'](_0x3f3ee3(241)) [_0x3f3ee3(257)] = _0x3f3ee3(240);
+};
+let totalCustomDotsContainer = 32,
+totalCustomDots = 42;
+function siteTemp() {
+  const _0x53a321 = _0x267948;
+  return _0x53a321(251);
+}((() =>{
+  const _0x420c72 = _0x267948,
+  _0x36458a = document[_0x420c72(242)](_0x420c72(258)),
+  _0x4918de = [
+  ];
+  for (let _0x36719e = 0; _0x36719e < totalCustomDotsContainer; _0x36719e++) {
+    let _0x415735 = '';
+    for (let _0x2af50d = 0; _0x2af50d < totalCustomDots; _0x2af50d++) {
+      _0x415735 += _0x420c72(243);
+    }
+    if (_0x36719e === 5) _0x4918de['push'](siteTemp());
+    _0x4918de[_0x420c72(264)](_0x420c72(253) + _0x415735 + _0x420c72(275));
+  }
+  _0x36458a['insertAdjacentHTML'](_0x420c72(271), _0x4918de[_0x420c72(232)](''));
+}) ());
+const allBoxes = document[_0x267948(255)](_0x267948(245)),
+arrow = _0x267948(239),
+removeArrow = () =>{
+  const _0x43b4b2 = _0x267948;
+  allBoxes[_0x43b4b2(270)](_0x5061c2=>{
+    const _0x1d74ff = _0x43b4b2;
+    _0x5061c2[_0x1d74ff(257)] = '';
+  });
+},
+boxes = [
+  _0x267948(229),
+  _0x267948(224),
+  _0x267948(260),
+  _0x267948(254)
+];
+let contentx = [
+  _0x267948(227),
+  _0x267948(238),
+  _0x267948(248),
+  atob(_0x267948(250))
+];
+function _0x5f26() {
+  const _0x491022 = [
+    'Embedded within the intricate codes of Mother\'s system lies the Alien Loader, a peculiar YAML loader function. This function parses and loads YAML data. Be cautious, as this loader holds the truths to unveil the hidden paths.',
+    'connect',
+    'Alien Loader',
+    'addEventListener',
+    '<p>',
+    'join',
+    '12783888REfoSA',
+    '452151GTewca',
+    '</p>',
+    'VEhNX0ZMQUd7MFJEM1JfOTM3fQ==',
+    'keyCode',
+    '[!]CAUTION[!] The Nostromo holds countless winding corridors and concealed chambers, harboring secrets that lie beyond the intended boundaries. Embrace the power of relative file paths within MOTHER, to uncover SECRETS and traverse the labyrinthine structure of the ship and reach your desired destinations.',
+    '\n<div class="absolute top-[50%] -right-[51px] w-[51px] flex items-center justify-center">\n<p class="theme-line w-[51px]"></p>\n</div>\n',
+    'Ash',
+    '.crew-member',
+    'querySelector',
+    '<p class="custom-dots__dots"></p>',
+    '/yaml',
+    '.button-box',
+    'log',
+    'nostromo',
+    'Crew Member',
+    '.content-placeholder',
+    'Q0xBU1NJRklFRA==',
+    '\n <div class="special-grid">\n  <div\n  class="w-full] rounded-lg flex items-center z-20 p-3 pb-0  shadow-lg flex-1"\n  >\n  <div class="flex flex-col w-[30%] justify-between h-full mr-[50px] text-center">\n    <div\n      class="p-4 theme-green rounded-lg relative button-box"\n      id="top"\n    > \n    <div class="absolute top-[50%] -right-[51px] w-[51px] flex items-center justify-center">\n      <p class="theme-line w-[51px]"></p>\n    </div>\n    Alien Loader\n    </div>\n    <div\n      class="p-4 theme-green rounded-lg relative button-box"\n      id="bottom"\n    > Pathways </div>\n    <div\n      class="p-4 theme-green rounded-lg relative button-box"\n      id="left"\n    > Role </div>\n    <div\n      class="p-4 theme-green rounded-lg relative button-box"\n      id="right"\n    > Flag </div>\n  </div>\n  <div class="flex justify-center flex-1 h-[20rem] overflow-y-auto descr-box \t overflow-x-hidden theme-green">\n    <div\n      class=" rounded-lg py-2 px-4  flex gap-4 items-center justify-center mr-1"\n    >\n      <div class="content-placeholder">\n        <p>\n          Embedded within the intricate codes of Mother\'s system lies the Alien Loader, a peculiar\n          YAML loader function. This function parses and loads YAML data. Be cautious, as this\n          loader holds the truths to unveil the hidden paths.\n        </p>\n      </div>\n    </div>\n  </div>\n  </div>\n  <div class="flex w-full mx-auto items-center z-20 gap-2 pl-2 shadow-lg theme-color" >\n    <p class="text-lg">Use</p>\n    <b class="text-xl">UP</b>\n    <p class="text-lg">and</p>\n    <b class="text-xl">DOWN</b>\n    <p class="text-lg">keys to move.</p>\n  </div>\n</div>\n  ',
+    'keydown',
+    '<div class="custom-dots">',
+    'Flag',
+    'querySelectorAll',
+    'Connected to /nostromo route',
+    'innerHTML',
+    '.dots-container',
+    'which',
+    'Role',
+    '2YXlRBI',
+    '6035552fsqHMF',
+    '769345BuEAYr',
+    'push',
+    '4122108yFVOqX',
+    '327550PsrtFg',
+    'Connected to /yaml route',
+    'insertAdjacentHTML',
+    'yaml',
+    'forEach',
+    'afterbegin',
+    'disconnect',
+    'Disconnected from /yaml route',
+    '1591329qUpoub',
+    '</div>',
+    '126GvQOEt',
+    'Pathways',
+    '/nostromo',
+    'Disconnected from /nostromo route'
+  ];
+  _0x5f26 = function () {
+    return _0x491022;
+  };
+  return _0x5f26();
+}
+const addArrow = _0x1005d3=>{
+  const _0x1094b2 = _0x267948;
+  let _0x205dbb = contentx;
+  const _0x48ef0b = document[_0x1094b2(242)](_0x1094b2(249));
+  allBoxes[_0x1094b2(270)]((_0x5b4433, _0x40d7ad) =>{
+    const _0x1e5f91 = _0x1094b2;
+    _0x5b4433['innerHTML'] = boxes[_0x40d7ad],
+    _0x1005d3 == _0x40d7ad && (_0x5b4433[_0x1e5f91(268)](_0x1e5f91(271), arrow), _0x48ef0b[_0x1e5f91(257)] = '', _0x48ef0b[_0x1e5f91(268)](_0x1e5f91(271), _0x1e5f91(231) + _0x205dbb[value] + _0x1e5f91(235)));
+  });
+};
+let value = 0;
+document[_0x267948(230)](_0x267948(252), function (_0x24853f) {
+  const _0x31f3e6 = _0x267948;
+  var _0x4fa5b9 = _0x24853f[_0x31f3e6(237)] || _0x24853f[_0x31f3e6(259)];
+  switch (_0x4fa5b9) {
+    case 38:
+      value = value - 1;
+      if (value < 0) value = 0;
+      removeArrow(),
+      addArrow(value);
+      break;
+    case 40:
+      value = value + 1;
+      if (value > 3) value = 3;
+      removeArrow(),
+      addArrow(value);
+      break;
+    default:
+      return;
+  }
+});
